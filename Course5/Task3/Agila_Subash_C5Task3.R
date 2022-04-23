ed477f42f4ba2e72a6281407a29f69c604cde467
Agila Subash <agilas@gmail.com>
2022-04-22 21:52:31 -0500

    updating LargeMatrix

--- Course5/Task3/Agila_Subash_C5Task3.R
+++ Course5/Task3/Agila_Subash_C5Task3.R
@@ -27,8 +27,10 @@ install.packages("C50")
 install.packages("kknn")
 install.packages("gbm")
 install.packages("AppliedPredictiveModeling")
-install.packages("tidtverse")
+install.packages("tidyverse")
 install.packages("plotly")
+install.packages("kernlab")
+install.packages('dplyr')
 
 
 #Library
@@ -44,7 +46,8 @@ library(AppliedPredictiveModeling)
 library (randomForest)
 library (doParallel)
 library(plotly)
-
+library(kernlab)
+library(dplyr)
 
 
 #---------------1. Set Up Parallel Processing-------------------
@@ -52,7 +55,7 @@ library(plotly)
 detectCores()
 
 #Create clusters with desired number of cores
-cl <- makeCluster(6)
+cl <- makeCluster(8)
 registerDoParallel(cl)
 
 #Get the number of cores are assigned for R and Rstudio
@@ -89,91 +92,193 @@ any(is.na(iphone_smallmatrix))
 
 #----------------3. Preprocessing & Feature Selection --------------
 
-#-----Correlation----------------
+# 1-----Correlation----------------
 #select relevant columns for iphone
 
-iphone_columns <- iphone_smallmatrix %>%
+#iphonetemp <- iphone_smallmatrix
+options(max.print = 1000000)
+tempCOR1 <- cor(iphone_smallmatrix)
+corrplot(tempCOR)
+
+iphoneCOR <- iphone_smallmatrix %>%
   select (starts_with("iphone"), starts_with("ios"), iphonesentiment)
 
 # Use the cor() and corrplot() functions
-options(max.print = 1000000)
-iphoneCOR<- cor(iphone_columns)
-corrplot(iphoneCOR)
+tempCOR2<- cor(iphoneCOR)
+corrplot(tempCOR2)
 
-#-----Examine Feature Variance----
+
+# 2-----Examine Feature Variance----
 
 ## Examine feature variance: nearZeroVar() with saveMetrics = TRUE 
 # returns an object containing a table including: 
 #frequency ratio, percentage unique, zero variance and near zero variance 
 
-nzvMetrics <- nearZeroVar(iphoneCOR, saveMetrics = TRUE)
+nzvMetrics <- nearZeroVar(iphone_smallmatrix, saveMetrics = TRUE)
 nzvMetrics
 
-nzv <- nearZeroVar(iphoneCOR, saveMetrics = FALSE)
+nzv <- nearZeroVar(iphone_smallmatrix, saveMetrics = FALSE)
+nzv
+
+# create a new data set and remove near zero variance features
+iphoneNZV <- iphone_smallmatrix[,-nzv]
+str(iphoneNZV)
+
  
 #--------Recursive Feature elimination---------
+
 #sample the data before using RFE
 set.seed(101)
 # Take 1000 sample and work through it.
-
 iphoneSample <- iphone_smallmatrix [sample(1:nrow(iphone_smallmatrix ), 1000, replace=FALSE),]
 
 #Set up rfeControl with randomforest, repeated cross validation and no updates
-
 ctrl <- rfeControl(functions= rfFuncs,
                    method= "repeatedcv",
                    repeats= 5,
                    verbose= FALSE)
 
 #Use rfe and omit the response variable (attribute 59 iphonesentiment)
-
 rfeResults <- rfe(iphoneSample[,1:58],
                   iphoneSample$iphonesentiment,
                   sizes =(1:58),
                   rfeControl=ctrl)
 
+rfeResults
+plot(rfeResults, type=c("g","o"))
+
+#create new data set with rfe recommended features
+iphoneRFE <- iphone_smallmatrix [,predictors(rfeResults)]
+
+# add the dependent variable to iphoneRFE
+iphoneRFE$iphonesentiment <- iphone_smallmatrix$iphonesentiment
+str(iphoneRFE)
+varImp(rfeResults)
+
+#-----------Preprocessing---------------------
+
+# Factorize the dependent variable 
+iphone_smallmatrix$iphonesentiment <- as.factor(iphone_smallmatrix$iphonesentiment)
+iphoneNZV$iphonesentiment <- as.factor(iphoneNZV$iphonesentiment)
+iphoneRFE$iphonesentiment <- as.factor(iphoneRFE$iphonesentiment)
+
+str(iphone_smallmatrix$iphonesentiment)
 
 
-#Training Set
-summary(iphone_sample)
-str(iphone_sample)
-head(iphone_sample)
-tail(iphone_sample)
+#---4. Model Development and Evaluation----------------------------
 
-#change variable types
-iphone_sample$iphonesentiment <- as.factor(iphone_sample$iphonesentiment)
-
-#normalize
-
-preprocessParams <- preProcess(iphone_sample[,1:58], method= c("center", "scale"))
-print(preprocessParams)
-
-iphone_sample_N <-predict(preprocessParams,iphone_sample )
-str(iphone_sample_N)
-ncol(iphone_sample_N)
+#---------- Out of the Box Model Development------------------------
 
 
-#------------------Prediction Set------------------------
+# Define an 70%/30% train/test split of the iphoneDF
+inTraining <- createDataPartition(iphone_smallmatrix$iphonesentiment, p = .70, list = FALSE)
+training <- iphone_smallmatrix[inTraining,]
+testing <- iphone_smallmatrix[-inTraining,]
 
-summary(largematrix)
-str(largematrix)
-head(largematrix)
-nrow(largematrix)
-ncol(largematrix)
+# 10 fold cross validation 
+fitControl <- trainControl(method = "cv", number = 10)
 
-is.na(largematrix)
-any(is.na(iphone_smallmatrix))
+#--------- C5.0 training--------------
+C50 <- train(iphonesentiment~., data = training, method = "C5.0", trControl=fitControl)
+# Testing 
+prediction_C50 <- predict(C50, testing)
 
-#drop ID variable
-largematrix$id <- NULL
+plot(C50)
 
-#change variable types
 
-largematrix$iphonesentiment <- as.factor (largematrix$iphonesentiment)
+##--------- Random Forest Train--------------
+rf <- train(iphonesentiment~., data = training, method = "rf", trControl=fitControl)
+# Testing 
+prediction_rf<- predict(rf, testing)
 
-ncol(largematrix)
-ncol(iphone_sample_N)
+plot(rf)
+##-------------SVM with 10-fold cross validation ------
+svm <- train(iphonesentiment~., data = training, method = "svmLinear", trControl=fitControl)
+# Testing 
+prediction_svm<- predict(svm, testing)
 
+plot(svm)
+###------------- KKNN with 10-fold cross validation------
+kknn <- train(iphonesentiment~., data = training, method = "kknn", trControl=fitControl)
+# Testing 
+prediction_kknn<- predict(kknn, testing)
+plot(kknn)
+
+
+#---------- Model Performance----------------
+
+# Evaluate C5.0 Model
+postResample(prediction_C50, testing$iphonesentiment)
+# Evaluate RF Model
+postResample(prediction_rf, testing$iphonesentiment)
+# Evaluate SVM Model
+postResample(prediction_svm, testing$iphonesentiment)
+# Evaluate KKNN Model
+postResample(prediction_kknn, testing$iphonesentiment)
+
+
+#------Confusion Metrics---------------
+
+# Create a confusion matrix from random forest predictions 
+CMRF <- confusionMatrix(prediction_rf, testing$iphonesentiment) 
+CMRF
+
+CM_C50 <- confusionMatrix(prediction_C50, testing$iphonesentiment)
+CM_C50
+
+
+#-RF (Best performing model) for iphoneNZV,iphoneRFE,
+
+# Define an 70%/30% train/test split of the iphoneNZV
+inTraining_iphoneNZV <- createDataPartition(iphoneNZV$iphonesentiment, p = .70, list = FALSE)
+training_NZV <- iphoneNZV[inTraining,]
+testing_NZV <- iphoneNZV[-inTraining,]
+
+# Apply RandomForest with 10-fold cross validation on iphoneNZV
+rf_NZV <- train(iphonesentiment~., data = training_NZV, method = "rf", trControl=fitControl)
+
+# Testing 
+prediction_rf_NZV<- predict(rf_NZV, testing_NZV)
+
+#---------------
+
+
+# Define an 70%/30% train/test split of the iphoneRFE
+inTraining_iphoneRFE <- createDataPartition(iphoneRFE$iphonesentiment, p = .70, list = FALSE)
+training_RFE <- iphoneRFE[inTraining,]
+testing_RFE <- iphoneRFE[-inTraining,]
+
+# Apply RandomForest with 10-fold cross validation on iphoneRFE
+rf_RFE <- train(iphonesentiment~., data = training_RFE, method = "rf", trControl=fitControl)
+# Testing 
+prediction_rf_RFE<- predict(rf_RFE, testing_RFE)
+
+#---------------------- PostResample NZV RFE--------------
+
+postResample(prediction_rf_NZV, testing_NZV$iphonesentiment)
+postResample(prediction_rf_RFE, testing_RFE$iphonesentiment)
+
+
+#-------------5.Feature Engineeering----------------------
+iphoneRC <- iphone_smallmatrix
+
+# Recode sentiment to combine factor levels 0 & 1 and 4 & 5
+iphoneRC$iphonesentiment <- recode(iphoneRC$iphonesentiment, '0' = 1, '1' = 1, '2' = 2, '3' = 3, '4' = 4, '5' = 4) 
+
+# Make iphonesentiment a factor
+iphoneRC$iphonesentiment <- as.factor(iphoneRC$iphonesentiment)
+
+str(iphoneRC$iphonesentiment)
+
+# Define an 70%/30% train/test split of the iphoneRC
+inTrainingRC <- createDataPartition(iphoneRC$iphonesentiment, p = .70, list = FALSE)
+training_RC <- iphoneRC[inTraining,]
+testing_RC <- iphoneRC[-inTraining,]
+
+# Use The Best RandomForest with 10-fold cross validation on Recoding the Dependant variable
+rf_RC <- train(iphonesentiment~., data = training_RC, method = "rf", trControl=fitControl)
+# Testing 
+prediction_rf_RC<- predict(rf_RC, testing_RC) 
 
 
 #stop cluster after performing tasks.
