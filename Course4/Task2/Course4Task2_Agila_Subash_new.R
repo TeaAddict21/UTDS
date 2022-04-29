a6623a81dc709e20bb792ad94c2e2db9322b599d
Agila Subash <agilas@gmail.com>
2022-04-29 06:24:21 -0500

    C4 Task2 update

--- Course4/Task2/Course4Task2_Agila_Subash_new.R
+++ Course4/Task2/Course4Task2_Agila_Subash_new.R
@@ -224,7 +224,8 @@ plot_ly(houseDay10, x = ~houseDay10$DateTime,
   add_trace(y = ~houseDay10$Sub_metering_3,
             name = 'Water Heater & AC',
             mode = 'lines') %>%
-  layout(title = "Power Consumption January 9th, 2008",
+  layout(title = "Power Consumption January 9th, 2008
+         10 min frequency",
          xaxis = list(title = "Time"),
          yaxis = list (title = "Power (watt-hours)"))
 
@@ -246,7 +247,8 @@ plot_ly(houseJanWeek4, x = ~houseJanWeek4$DateTime,
   add_trace(y = ~houseJanWeek4$Sub_metering_3,
             name = 'Water Heater & AC',
             mode = 'lines') %>%
-  layout(title = "Power Consumption Week 2 of Jan, 2008",
+  layout(title = "Power Consumption Week 2 of Jan, 2008
+         every 20 min frequency",
          xaxis = list(title = "Time"),
          yaxis = list (title = "Power (watt-hours)"))
 
@@ -307,8 +309,9 @@ temptest1 <- updatedDF %>%
            yaxis = list (title = "Power (watt-hours)"))
   
 
-### Time Series Analysis ##############
-  ## Submeter_1
+####################### Time Series Analysis ##########################
+
+   ## Submeter_3
 #Subset to one observation per week on Mondays at 8:00 P.M for 2007, 2008, 2009
   
 house070809weekly <- filter(updatedDF, weekday==2 & hour== 20 & minute ==1)
@@ -329,64 +332,34 @@ autoplot(tsSM3_070809weekly,
          main="Sub_meter 3")
 
 
+#---------------------------------------
+### Every Saturday for SubMeter 1 (kitchen)
 
-  ##Submeter_ 1 (kitchen)
-## Method 1 : taking data during the peak hour based on usage data
-#Subset to one observation per week on Mondays between 6:00  and 9:00 P.M for 2/2008
+# Subset to 1 observation per week on Saturday at 8:00pm for 2007, 2008 and 2009
+house070809weekly_Sat <- filter(updatedDF, weekday == 7 & hour == 20 & minute == 1)
 
-houseEverydayKitchen <- filter(updatedDF, year == 2008 & month==2 &
-                                 (hour >17 & hour < 20)  
-                                 )
+# Create TS object with SubMeter 1
+tsSM1_070809weekly_Sat <- ts(house070809weekly_Sat$Sub_metering_1,
+                             frequency=52, 
+                             start=c(2007,1))
 
-#create TS object with Submeter_1 (kitchen)
+# Plot sub-meter 1 with plot.ts
+plot.ts(tsSM1_070809weekly_Sat, main = "Time Series Analysis
+            Sub-meter 1-Kitchen",
+             col="dark green")
 
-tsSM1_EverydayKitchen <- ts(houseEverydayKitchen$Sub_metering_1 ,
-                         frequency = 3479, 
-                         start= c(200802,1)
-                    
-                         )
 
-tsSM1_EverydayKitchen
-
-autoplot(tsSM1_EverydayKitchen,
-         color = 'blue',
-         xlab="Time",
-         ylab="Watt Hours",
-         main="Sub_meter 1 Kitchen")
-
-## Method 1 : Summarizing the monthly data for all three years and using the data
-
-houseUsage_monthly <- updatedDF %>%
-  group_by(year(DateTime), month(DateTime)) %>%
-  summarise(Sub_meter_1 = round(sum(`Sub_metering_1`/1000),3),
-            Sub_meter_2 = round(sum(`Sub_metering_2`/1000),3),
-            Sub_meter_3 = round(sum(`Sub_metering_3`/1000),3)
-            )
-  
-head(houseUsage_monthly)
-
-tsSM1_montlyKitchen <- ts(houseUsage_monthly$Sub_meter_1,
-                          frequency = 12,
-                          start= c(2008,1)
-                                )
-
-autoplot(tsSM1_montlyKitchen, 
-         color='purple',
-         xlab="time",
-         ylab="Watt Hours",
-         main = "Sub_meter 1 Kitchen")
 
 
 
 ## Submeter_2 (Laundry Room)
 
-house070809daily <- filter(updatedDF, hour==20 & minute==1)
+house070809daily <- filter(updatedDF,weekday==1 & hour==20 & minute==1)
 
-
-tsSM2_daily <- ts(house070809daily$Sub_metering_2, frequency = 365, start=c(2007,1))
+tsSM2_daily <- ts(house070809daily$Sub_metering_2, frequency = 52, start=c(2007,1))
 
 autoplot(tsSM2_daily,
-         ts.colour = 'orange',
+         colour = 'orange',
          xlab="Time",
          ylab="Watt Hours",
          main="Sub_meter 2 Laundry")
@@ -395,26 +368,17 @@ autoplot(tsSM2_daily,
 #### End Time series Analysis###########
 
 
+
+
 #### Start : Forecasting time series###########
 
 ##Sub_meter 3 ##
-
-
 ##Apply time series linear regression to the sub-meter 3 ts object and
 # use summary to obtain R2 and RMSE from the model you built
 
 
 fitSM3 <- tslm(tsSM3_070809weekly ~ trend + season)
 summary(fitSM3)
-glance(fitSM3)
-
-
-## Create the forecast for sub-meter 3. Forecast ahead 20 time periods 
-
-forecastfitSM3 <- forecast(fitSM3, h=20)
-
-# Plot the forecast for sub_meter 3
-plot(forecastfitSM3)
 
 ## Create the forecast for sub-meter 3. Forecast ahead 20 time periods 
 # with confidence levels 80 and 90
@@ -423,34 +387,134 @@ forecastfitSM3_withC <- forecast(fitSM3, h=20, level= c(80,90))
 
 #plot sub_meter 3 forecast, limit y and add labels
 
-plot(forecastfitSM3_withC, ylim= c(0,20), ylab= "Watt_Hours", xlab= "Time")
+plot(forecastfitSM3_withC, ylim= c(0,20), ylab= "Watt_Hours", xlab= "Time", 
+     main= "AC & Heater Mondays at 8:00 P.M")
 
 ## Sub _meter 1###
 
-##Apply time series linear regression to the sub-meter 1 (kitchen) ts object and
-# use summary to obtain R2 and RMSE from the model you built
-
-fitSM1 <- tslm(tsSM1_montlyKitchen ~ trend + season)
+# My trial (weekly--"Saturday") for Sub-meter 1 
+# Subset to 1 observation per week on Saturday at 8:00pm for 2007, 2008 and 2009
+fitSM1 <- tslm(tsSM1_070809weekly_Sat ~ trend + season) 
 summary(fitSM1)
-glance(fitSM1)
 
-forecastfitSM1 <- forecast(fitSM1,h=20)
-plot(forecastfitSM1)
+# Create Sub-meter 1 forecast with confidence levels 80 and 90
+forecastfitSM1c <- forecast(fitSM1, h=24, level=c(80,90))
+# Plot sub-meter 3 forecast, limit y and add labels
+plot(forecastfitSM1c, ylim = c(0, 20), ylab= "Watt-Hours", xlab="Time",
+     main = "Kitchen Forecast 2007-2009 Every Saturday 8:00pm")
+
+### Sub-Meter 2 (2007-2009)
+
+# daily data for 365 days, taken at 18:00pm per day
+fitSM2 <- tslm(tsSM2_daily ~ trend + season) 
+summary(fitSM2)
+
+# Create sub-meter 2 forecast &  forecast ahead 24 (1 day) period with confidence levels 80 and 90
+forecastfitSM2c <- forecast(fitSM2, h=24, level=c(80,90))
+# Plot sub-meter 2 forecast, limit y and add labels
+plot(forecastfitSM2c, ylim = c(0, 20), ylab= "Watt-Hours", xlab="Time", 
+     main = "Laundry Room Forecast 2007-2009 Daily 8:00pm")
   
-# Create the forecast for sub-meter 1 (kitchen). forecast ahead 30 time periods
 
-forecastfitSM1 <- forecast(fitSM1, h=20, c(80,90))
-plot(forecastfitSM1)
+##### End ForeCasting  ############
 
-plot(forecastfitSM1, ylim= c(0,90), ylab= "Watt_Hours", xlab= "Time")
+###--------Decomposing a Seasonal Time Series-----------##
 
 
+# Decompose Sub-meter 3 into trend, seasonal and random
+components070809SM3weekly <- decompose(tsSM3_070809weekly)
+# Plot decomposed sub-meter 3 
+plot(components070809SM3weekly)
 
+summary(components070809SM3weekly)
+
+#---------------
+#Sub meter1
+# Decompose Sub-meter 1 into trend, seasonal and random
+components070809SM1weekly_Sat <- decompose(tsSM1_070809weekly_Sat)
+# Plot decomposed sub-meter 1 
+plot(components070809SM1weekly_Sat)
+summary(components070809SM1weekly_Sat)
+
+
+# Sub Meter 2
+
+# Decompose Sub-meter 2 into trend, seasonal and random
+components070809SM2daily <- decompose(tsSM2_daily) 
+# Plot decomposed sub-meter 2
+plot(components070809SM2daily)
+
+summary(components070809SM2daily)
+
+######--- End Decomposing a Seasonal Time Series-------#
+
+
+####----------------Holt-Winters Forecasting (Start) ----------------###
+
+# Remove Seasonal Components
+# Seasonal adjusting sub-meter 3 by subtracting the seasonal component & plot
+
+# Sub_meter 3:
   
+tsSM3_070809Adjusted <- tsSM3_070809weekly - components070809SM3weekly$seasonal
+
+autoplot(tsSM3_070809Adjusted)
+
+# Test Seasonal Adjustment by running Decompose again 
+plot(decompose(tsSM3_070809Adjusted))
 
 
+# Holt Winters Exponential Smoothing
+tsSM3_HW070809 <- HoltWinters(tsSM3_070809Adjusted, beta= FALSE, gamma= FALSE)
+plot(tsSM3_HW070809, ylim= c(0,25))
+# Forecast HoltWinters with confidence levels
+tsSM3_HW070809for <- forecast(tsSM3_HW070809, h=25)
+# Plot only the forecasted area
+plot(tsSM3_HW070809for, ylim = c(0, 20), ylab= "Watt-Hours", xlab="Time - Sub-meter 3",
+      main = "Sub-meter 3 HWForecast, Mondays, 20:00")
+
+## Forecast HoltWinters with diminished confidence levels
+tsSM3_HW070809forC <- forecast(tsSM3_HW070809, h=25, level=c(10,25))
+## Plot only the forecasted area
+plot(tsSM3_HW070809forC, ylim = c(0, 10), ylab= "Watt-Hours", xlab="Time - Sub-meter 3", start(2010))
 
 
+#-----------------
+
+# Sub_meter1: (Kitchen)
+
+
+# Seasonal adjusting sub-meter 1 by subtracting the seasonal component & plot
+tsSM1_070809Adjusted <- tsSM1_070809weekly_Sat - components070809SM1weekly_Sat$seasonal
+
+## Holt Winters Simple Exponential Smoothing & Plot
+tsSM1_HW070809 <- HoltWinters(tsSM1_070809Adjusted, beta=FALSE, gamma=FALSE)
+plot(tsSM1_HW070809, ylim = c(0, 25))
+
+tsSM1_HW070809forC <- forecast(tsSM1_HW070809, h=25, level=c(10,25))
+
+## Plot only the forecasted area
+plot(tsSM1_HW070809forC, ylim = c(0, 35), ylab= "Watt-Hours", xlab="Time - Sub-meter 1",
+     start(2010), main = "Kitchen HoltWinters Forecast, Every Saturday 8:00pm") 
+
+
+#-------------
+
+# Sub_meter 2 (Laundry Room)
+
+# My trail Sub-Meter 2 (daily data for 365 days and from 2007-2009, taken at 18:00pm per day)
+# Seasonal adjusting sub-meter 2 by subtracting the seasonal component & plot
+tsSM2_070809Adjusted <- tsSM2_daily - components070809SM2daily$seasonal
+
+# Holt Winters Triple Exponential Smoothing & Plot
+tsSM2_HW070809 <- HoltWinters(tsSM2_070809Adjusted) 
+plot(tsSM2_HW070809, ylim = c(0, 25))
+
+# Forecast HoltWinters with 80-90 confidence levels
+tsSM2_HW070809forC <- forecast(tsSM2_HW070809, h=25, level=c(10,25))
+# Plot only the forecasted area
+plot(tsSM2_HW070809forC, ylim = c(0, 3), ylab= "Watt-Hours", xlab="Time - Sub-meter 2", 
+     start(2010), main = "Laundry Room HoltWinters Forecast, Everyday 8:00pm")
 
 
 
